e4c31e2027ee21acd24556b39144584e67cf301a
Michael Williams <williamsmikeyj@outlook.com>
2024-03-06 20:55:37 -0500

    main for running Lexer

--- teenytiny.py
+++ teenytiny.py
@@ -1,2 +1,11 @@
 from lex import *
 
+def main():
+    source = 'LET foobar = 123'
+    lexer = Lexer(source)
+    
+    while lexer.peek() != '\0':
+        print(lexer.cur_char)
+        lexer.next_char()
+        
+main()
